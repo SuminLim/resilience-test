9bcf6fb2080efa7ebfc5dff100e3be6258b73ef4
SuMin.Lim <jason.lim@frientrip.com>
2020-01-11 19:51:51 +0900

    Updates

---
+++ precache-manifest.0ef2c72d2aaefa00381caddc98592747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fef0b4aade9758cb051a1cf9c52887d",
+    "url": "/resiliece-test/index.html"
+  },
+  {
+    "revision": "496c9b544f4b4fa8571f",
+    "url": "/resiliece-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "93811f8165d7f7686eb9",
+    "url": "/resiliece-test/static/js/2.ac7be6ba.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/resiliece-test/static/js/2.ac7be6ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "496c9b544f4b4fa8571f",
+    "url": "/resiliece-test/static/js/main.dab1f36b.chunk.js"
+  },
+  {
+    "revision": "fa8f4c34dac9acb42840",
+    "url": "/resiliece-test/static/js/runtime-main.c0a4f6b6.js"
+  }
+]);
