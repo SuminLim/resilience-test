e4c33a5a83330606defabd2665baf4511667521d
SuMin.Lim <jason.lim@frientrip.com>
2020-02-02 19:59:24 +0900

    Update Home.tsx
    Home ui 보강

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,24 @@ import React from 'react';
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Header = styled.h1`
+  text-align: left;
+`;
+
+const Divider = styled.hr`
+  width: 50vw;
+`;
+
+const Description = styled.p`
+  text-align: left;
+`;
+
 const ButtonStart = styled(Link)`
   width: 170px;
   height: 70px;
@@ -16,11 +34,26 @@ const ButtonStart = styled(Link)`
 
 const HomePage = () => {
   return (
-    <div>
-      <ButtonStart to="/test">
-        테스트 시작!
-      </ButtonStart>
-    </div>
+    <Wrapper>
+      <Header>
+        나의 회복탄력성 지수는?<br/>
+        &lt;KRQ-53 테스트&gt;
+      </Header>
+      <Divider/>
+      <div>
+        <Description>
+          <strong>응답 방법: </strong>
+          각 문항을 읽은 후 다음과 같이 점수를 기록한다.
+        </Description>
+        <Description>
+          전혀 그렇지 않다 1 / 그렇지 않다 2 / 보통이다 3 / 어느 정도 그렇다 4 / 매우 그렇다 5
+        </Description>
+
+        <ButtonStart to="/test">
+          테스트 시작!
+        </ButtonStart>
+      </div>
+    </Wrapper>
   );
 };
 
